82af00e28124ae16662c4d76405153a8b7842ab6
Artem Zhyrnyi <ic3ego@gmail.com>
2026-03-13 14:16:30 +0200

    add ANSI colours

--- main.go
+++ main.go
@@ -5,18 +5,20 @@ import (
 	"net/http"
 )
 
+const (
+	green = "\033[32m"
+	reset = "\033[0m"
+)
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/", home)
 
+	log.Printf("%sServer starting on http://localhost:4000/%s", green, reset)
 	err := http.ListenAndServe(":4000", mux)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(err)
 }
