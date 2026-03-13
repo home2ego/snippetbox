ea304529ba199b65ec2d38a0361d962ee5b51c8d
Artem Zhyrnyi <ic3ego@gmail.com>
2026-03-13 14:31:08 +0200

    add snippet view and create handler functions

--- main.go
+++ main.go
@@ -11,12 +11,22 @@ const (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	w.Write([]byte("Dispaly the home page"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Printf("%sServer starting on http://localhost:4000/%s", green, reset)
 	err := http.ListenAndServe(":4000", mux)
