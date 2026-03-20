384eed877eb6dc3b32e2c225b19e458b67d0abd3
Artem Zhyrnyi <ic3ego@gmail.com>
2026-03-20 23:50:10 +0200

    disable directory listings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,45 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+// Disable directory listings
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("GET /static", http.NotFoundHandler())
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
